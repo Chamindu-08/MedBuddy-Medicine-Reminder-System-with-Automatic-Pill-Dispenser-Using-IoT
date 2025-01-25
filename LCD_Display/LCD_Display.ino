5253c50e1573c59c54681372a4476db8fa00337b
Chamindu-08 <chamindu08induwara@gmail.com>
2025-01-25 21:59:05 +0530

    check buzzer and push button

--- LCD_Display/LCD_Display.ino
+++ LCD_Display/LCD_Display.ino
@@ -1,29 +1,21 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Initialize the library by specifying the LCD address (0x27 for a 16x2 LCD)
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup() 
-{
-    lcd.begin(); // Initialize the LCD
-    lcd.backlight(); // Turn on the backlight
+void setup() {
+    Wire.begin(4, 5); // SDA = GPIO4 (D2), SCL = GPIO5 (D1)
+
+    lcd.begin();
+    lcd.backlight();
 }
 
-void loop() 
-{
-    lcd.setCursor(0, 0); // Set cursor to first row, first column
-    lcd.print(" Tech Maker "); // Print 16 characters
-    delay(1000); // Delay for 1 second
+void loop() {
+    lcd.setCursor(0, 0);
+    lcd.print(" MedBuddy ");
+    delay(1000);
 
-    lcd.setCursor(0, 1); // Set cursor to second row, first column
-    lcd.print("Like | Share"); // Print message
-    delay(8000); // Delay for 8 seconds
-
-    lcd.clear(); // Clear the display
-    lcd.setCursor(0, 0); // Set cursor to first row, first column
-    lcd.print(" SUBSCRIBE "); // Print message
-    lcd.setCursor(0, 1); // Set cursor to second row, first column
-    lcd.print(" TECH MAKER "); // Print message
-    delay(8000); // Delay for 8 seconds
+    lcd.setCursor(0, 1);
+    lcd.print("Group 02");
+    delay(2000);
 }
