60bfe619cf1844e43a24f22fce32dd5df4b3e2a0
Chamindu-08 <chamindu08induwara@gmail.com>
2025-01-28 22:44:49 +0530

    initialize RTC

--- BedBuddy/BedBuddy.ino
+++ BedBuddy/BedBuddy.ino
@@ -1,11 +1,13 @@
 #include <ESP8266Firebase.h>
 #include <ESP8266WiFi.h>
+#include <RTClib.h>
 
 #define _SSID "gift"
 #define _PASSWORD ""
 #define REFERENCE_URL "https://medbuddy-85345-default-rtdb.firebaseio.com/"
 
 Firebase firebase(REFERENCE_URL);
+RTC_DS3231 rtc;
 
 void setup() {
   Serial.begin(9600);
@@ -29,6 +31,18 @@ void setup() {
   } else {
     Serial.println("Firebase Connection Failed!");
     Serial.println(firebase.error());
+  }
+
+  //initialize RTC
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  //check if RTC lost power and set time
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, setting the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 }
 
