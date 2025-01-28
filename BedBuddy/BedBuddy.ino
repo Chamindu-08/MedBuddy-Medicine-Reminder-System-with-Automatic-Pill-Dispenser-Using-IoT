eea4723283caa08c028cb82d336f815126ab3dcb
Chamindu-08 <chamindu08induwara@gmail.com>
2025-01-28 23:09:10 +0530

    servo motor initialization

--- BedBuddy/BedBuddy.ino
+++ BedBuddy/BedBuddy.ino
@@ -1,7 +1,9 @@
 #include <ESP8266Firebase.h>
 #include <ESP8266WiFi.h>
 #include <RTClib.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 
 #define _SSID "gift"
 #define _PASSWORD ""
@@ -10,6 +12,15 @@
 Firebase firebase(REFERENCE_URL);
 RTC_DS3231 rtc;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+//servo motor setup
+Servo servoA;
+Servo servoB;
+Servo servoC;
+const int servoPinA = 12;  // D6 -> GPIO 12
+const int servoPinB = 13;  // D7 -> GPIO 13
+const int servoPinC = 15;  // D8 -> GPIO 15
+const int startAngle = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -52,6 +63,14 @@ void setup() {
     Serial.println("RTC lost power, setting the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
+
+  //servo motor initialization
+  servoA.attach(servoPinA);
+  servoB.attach(servoPinB);
+  servoC.attach(servoPinC);
+  servoA.write(startAngle);
+  servoB.write(startAngle);
+  servoC.write(startAngle);
 }
 
 void loop() {
